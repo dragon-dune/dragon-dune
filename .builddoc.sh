5abcb4af81b0ec3a5b37486ad9883427a8d17034
dragon-dune <dragon.dune.game@gmail.com>
2017-06-06 06:39:59 -0700

    Update .builddoc.sh

--- .builddoc.sh
+++ .builddoc.sh
@@ -1,3 +1,6 @@
+echo "Document Building Script"
+echo "All documents are build from dragon-dune.odt"
+echo "Uses pandoc and libreoffice"
 pandoc dragon-dune.odt -o dragon-dune.md
 libreoffice --headless --convert-to pdf dragon-dune.odt
 cat "dragon-dune text board.txt" >> dragon-dune.md
